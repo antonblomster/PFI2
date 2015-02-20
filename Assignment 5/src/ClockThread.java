d437b7b3d40fbba1154c533dbfb80767fd67492a
Anton <Anton@172.20.10.3>
2015-02-20 15:01:47 +0100

    Klar!

---
+++ Assignment 5/src/ClockThread.java
@@ -0,0 +1,28 @@
+import java.util.Calendar;
+
+
+public class ClockThread extends Thread {
+
+	private ClockInterface clockInterface; 
+	
+	public ClockThread(ClockInterface ci){
+		this.clockInterface = ci; 
+		
+	}
+
+	public void run() {
+		while (true){
+			
+			Calendar cal = Calendar.getInstance();
+			clockInterface.update(cal.get(Calendar.HOUR_OF_DAY),(cal.get(Calendar.MINUTE)), (cal.get(Calendar.SECOND)));
+		
+			try {
+				Thread.sleep(900);
+			} catch (InterruptedException e) {
+
+				e.printStackTrace();
+			}	
+	}
+		}
+	
+}
